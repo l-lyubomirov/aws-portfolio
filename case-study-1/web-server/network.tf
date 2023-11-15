17b3518a6e295b4eb131c82bf24dcb0b3fc81d85
lyubomir-lyubomirov <lyubomir-lyubomirov@lubooooo2@abv.bg>
2023-11-15 23:21:06 +0200

    Added security groups and NACL in the network.tf file.

--- case-study-1/web-server/network.tf
+++ case-study-1/web-server/network.tf
@@ -102,3 +102,76 @@ resource "aws_route_table_association" "pr-subnet-route-table-association" {
   subnet_id      = aws_subnet.pr-subnet-cl.id
   route_table_id = aws_route_table.pr-rt-cl.id
 }
+
+resource "aws_security_group" "securitygroup-cl" {
+  vpc_id = aws_vpc.vpc-cl.id
+  name   = "security-group-case-lyubo"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["178.254.247.173/32"]
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "security-group-case-lyubo"
+  }
+}
+
+resource "aws_security_group" "ws-securitygroup-cl" {
+  vpc_id = aws_vpc.vpc-cl.id
+  name   = "ws-security-group-case-lyubo"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.securitygroup-cl.id]
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ws-security-group-case-lyubo"
+  }
+}
+
+resource "aws_network_acl" "nacl-cl" {
+  vpc_id = aws_vpc.vpc-cl.id
+
+  egress {
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+}
