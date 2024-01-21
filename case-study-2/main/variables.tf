256872ae5152357162bf86cbfcf98fc735693059
lyubomir-lyubomirov <lyubomir-lyubomirov@lubooooo2@abv.bg>
2024-01-21 12:35:59 +0200

    Added variables with default values for the second module in the module invocation.

--- case-study-2/main/variables.tf
+++ case-study-2/main/variables.tf
@@ -101,3 +101,68 @@ variable "flow_logs_traffic_type" {
 variable "subnet-public-map" {
   default     = true
 }
+
+variable "ec2-bastion-config" {
+  default = {
+    ami           = "ami-03a68febd9b9a5403"
+    instance_type = "t3.micro"
+    key_name      = "ec2-ssh-key"
+  }
+}
+
+variable "health-check-alb" {
+  default = {
+    health-check = {
+      enabled             = true
+      interval            = 60
+      path                = "/"
+      protocol            = "HTTP"
+      timeout             = 5
+      healthy_threshold   = 5
+      unhealthy_threshold = 10
+      matcher             = "403,200"
+      port                = "traffic-port"
+    }
+  }
+}
+
+variable "ami-webserver" {
+  default     = "ami-03a68febd9b9a5403"
+}
+
+variable "webserver-instance-type" {
+  default     = "t3.micro"
+}
+
+variable "asg-config" {
+  default = {
+    min_size                  = 1
+    max_size                  = 2
+    desired_capacity          = 1
+    health_check_grace_period = 300
+  }
+}
+
+variable "alb-config" {
+  default = {
+    internal           = false
+    ip_adress_type     = "ipv4"
+    load_balancer_type = "application"
+  }
+}
+
+variable "alb-listener-config" {
+  default = {
+    port                = 80
+    protocol            = "HTTP"
+    default_action_type = "forward"
+  }
+}
+
+variable "alb-tg-config" {
+  default = {
+    port        = 80
+    protocol    = "HTTP"
+    target_type = "instance"
+  }
+}
